36b39f002c8f85d9be17b78af5da279a8409c64b
Cabbe <38203817+perlejr@users.noreply.github.com>
2024-12-02 18:47:26 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,43 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try {
+            File myFile = new File("src/main/resources/input.txt");
+            Scanner myReader = new Scanner(myFile);
+            List<Integer> lhs = new ArrayList<>();
+            List<Integer> rhs = new ArrayList<>();
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] words = data.split("   ");
+                lhs.add(Integer.parseInt(words[0]));
+                rhs.add(Integer.parseInt(words[1]));
+            }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            lhs.sort(null);
+            rhs.sort(null);
+            int myResult = 0;
+            for (int i = 0; i < lhs.size(); i++) {
+                int tmp = 0;
+                for (int j = 0; j < rhs.size(); j++) {
+                    if (Integer.compare(lhs.get(i), rhs.get(j)) == 0) {
+                        tmp++;
+                    }
+                }
+                myResult += lhs.get(i) * tmp;
+            }
+
+            System.out.println(myResult);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
         }
+
     }
 }
